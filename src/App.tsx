158e731dc1e190d43380a0afe9546d5a2212ad8f
exit <saraswathi.mac@gmail.com>
2023-03-25 12:12:57 +0530

    Added styles and logic for calender

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import { useState } from "react";
+import Calendar from "./components/Calender/Calender";
+
+const App = () => {
+  const [currentDate, ] = useState(new Date("10-03-2022"));
+  
   return (
-    <div className="App">
-      calender
+    <div className="mt-16 flex flex-col items-center gap-8">
+      <Calendar value={currentDate} />
     </div>
   );
-}
+};
 
 export default App;
